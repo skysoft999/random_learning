b0dea8c579dc5dd1fefd1e074a7b6b14b472beea
sanu k yadav <sky@sanus-MacBook-Pro.local>
2020-10-03 22:59:55 +0530

    flask restful api

--- rest_API/rest3/app.py
+++ rest_API/rest3/app.py
@@ -1,11 +1,10 @@
 from flask import Flask
+from flask_restful import Api
+from resources.routes import initialize_routes
 from database.db import initialize_db
-from resources.movie import movies
-
-# from database.models import Movie
-# import json
 
 app = Flask(__name__)
+api = Api(app)
 
 app.config['MONGODB_SETTINGS'] = {
     'host': 'mongodb://localhost/movie-bag'
@@ -13,6 +12,6 @@ app.config['MONGODB_SETTINGS'] = {
 
 initialize_db(app)
 
-app.register_blueprint(movies)
+initialize_routes(api)
 
 app.run()
---
+++ rest_API/rest3/resources/routes.py
@@ -0,0 +1,8 @@
+# ~movie-bag/resources/routes.py
+
+from .movie import MoviesApi, MovieApi
+
+
+def initialize_routes(api):
+    api.add_resource(MoviesApi, '/api/movies')
+    api.add_resource(MovieApi, '/api/movies/<id>')
