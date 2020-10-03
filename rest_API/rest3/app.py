4b1e09195c02a8e1ed1b4073e4f4b16716102e07
sanu k yadav <sky@sanus-MacBook-Pro.local>
2020-10-04 00:08:49 +0530

    complete jwt fix

--- rest_API/rest3/app.py
+++ rest_API/rest3/app.py
@@ -2,9 +2,16 @@ from flask import Flask
 from flask_restful import Api
 from resources.routes import initialize_routes
 from database.db import initialize_db
+from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
+
 
 app = Flask(__name__)
+app.config.from_envvar('ENV_FILE_LOCATION')
 api = Api(app)
+bcrypt = Bcrypt(app)
+
+jwt = JWTManager(app)
 
 app.config['MONGODB_SETTINGS'] = {
     'host': 'mongodb://localhost/movie-bag'
