1c22df54f5d863308378d5998ec29d75dffe708a
sanu k yadav <sky@sanus-MacBook-Pro.local>
2020-10-04 14:35:16 +0530

    api completion with testing

--- rest_API/rest3/app.py
+++ rest_API/rest3/app.py
@@ -1,19 +1,23 @@
 from flask import Flask
 from flask_restful import Api
-from resources.routes import initialize_routes
+# from resources.routes import initialize_routes
 from database.db import initialize_db
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from resources.errors import errors
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config.from_envvar('ENV_FILE_LOCATION')
+mail = Mail(app)
 api = Api(app, errors=errors)
 bcrypt = Bcrypt(app)
+from resources.routes import initialize_routes
 
 jwt = JWTManager(app)
 
-app.config['MONGODB_SETTINGS'] = {
+
+MONGODB_SETTINGS = {
     'host': 'mongodb://localhost/movie-bag'
 }
 
@@ -21,4 +25,4 @@ initialize_db(app)
 
 initialize_routes(api)
 
-app.run()
+# app.run()
---
+++ rest_API/rest3/tests/test_create_movie.py
@@ -0,0 +1,33 @@
+
+import json
+
+from tests.BaseCase import BaseCase
+
+class TestUserLogin(BaseCase):
+
+    def test_successful_login(self):
+        # Given
+        email = "test@gmail.com"
+        password = "test1234"
+        user_payload = json.dumps({
+            "email": email,
+            "password": password
+        })
+
+        self.app.post('/api/auth/signup', headers={"Content-Type": "application/json"}, data=user_payload)
+        response = self.app.post('/api/auth/login', headers={"Content-Type": "application/json"}, data=user_payload)
+        login_token = response.json['token']
+
+        movie_payload = {
+            "name": "Star Wars: The Rise of Skywalker",
+            "casts": ["Daisy Ridley", "Adam Driver"],
+            "genres": ["Fantasy", "Sci-fi"]
+        }
+        # When
+        response = self.app.post('/api/movies',
+            headers={"Content-Type": "application/json", "Authorization": f"Bearer {login_token}"},
+            data=json.dumps(movie_payload))
+
+        # Then
+        self.assertEqual(str, type(response.json['id']))
+        self.assertEqual(200, response.status_code)
